c9ab12dbb5d323dfa79fa3c9412e4fada972f529
Anas-Esbitan <anas.esbitan7@gmail.com>
2024-11-10 09:59:36 +0300

    Initial commit

---
+++ Hala.php
@@ -0,0 +1,17 @@
+<?php 
+include ("model/conn.php");
+$conn = new Database();
+$dbConnection = $conn->getConnection(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+</body>
+</html>
